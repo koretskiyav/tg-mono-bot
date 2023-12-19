25afbe0daf16a4800d0f5cec02c3bed1df92515d
Andrii Koretskyi <koretskiyav@gmail.com>
2023-12-19 23:31:17 +0200

    one more fix

--- src/db.ts
+++ src/db.ts
@@ -1,19 +1,15 @@
-import * as pg from 'pg';
 import { Sequelize } from "sequelize-typescript";
 import { User } from "./models/user";
 
-export const db = new Sequelize(
-  process.env.POSTGRES_DATABASE || "POSTGRES_DATABASE",
-  process.env.POSTGRES_USER || "POSTGRES_USER",
-  process.env.POSTGRES_PASSWORD || "POSTGRES_PASSWORD",
-  {
-    models: [User],
-    dialectModule: pg,
-    host: process.env.POSTGRES_HOST || "POSTGRES_HOST",
-    port: Number(process.env.POSTGRES_PORT) || 5432,
+export const db = new Sequelize({
+  database: process.env.POSTGRES_DATABASE || "POSTGRES_DATABASE",
+  dialect: "postgres",
+  username: process.env.POSTGRES_USER || "POSTGRES_USER",
+  password: process.env.POSTGRES_PASSWORD || "POSTGRES_PASSWORD",
+  host: process.env.POSTGRES_HOST || "POSTGRES_HOST",
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+  models: [User],
+  dialectOptions: {
     ssl: true,
-    dialectOptions: {
-      ssl: true,
-    }
-  }
-);
+  },
+});
