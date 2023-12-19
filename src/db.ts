41ae496ebea1ede4376732cf159eacc7fc37fd9c
Andrii Koretskyi <koretskiyav@gmail.com>
2023-12-19 16:24:10 +0200

    fix pg dialect

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,8 @@
+import * as pg from 'pg';
 import { Sequelize } from 'sequelize-typescript';
 import { User } from './models/user';
 
 const POSTGRES_URL = process.env.POSTGRES_URL || 'POSTGRES_URL';
 
-export const db = new Sequelize(POSTGRES_URL, { models: [User] })
+export const db = new Sequelize(POSTGRES_URL, { models: [User], dialectModule: pg })
 
